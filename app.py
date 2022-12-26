3b8045f342cec52655a7a950369853bd18834901
Jingxiang Mo <65676392+jingxiangmo@users.noreply.github.com>
2022-12-25 19:40:26 -0800

    UX update and code cleanup

--- app.py
+++ app.py
@@ -24,36 +24,32 @@ def generate_translation(user_input, lang):
   )  
   return response.choices[0].text
 
-input_translate = ""
-
+def get_rewrite():
+  input_rewrite = st.text_area("Email to rewrite: ", input_translate,  key="rewrite text area")
+  return input_rewrite
 
 def get_translate(lang):
   input_translate=st.text_area('Text to translate:', '''''', key="translate text area")
   if input_translate:
     return generate_translation(input_translate, lang)
 
-st.header("Translator")
-choice = st.selectbox("Translation format:", ["Simplified Chinese -> English", "English -> Simplified Chinese"])
 
-input_translate = get_translate(choice)
+# ==========[ STREAMLIT ]========== # 
+st.set_page_config(layout="wide")
+col1, col2 = st.columns(2)
 
-if st.button('Start translating', key="translate button"):
-  if input_translate:
-      st.markdown(input_translate)
+with col1:
+  st.header("Translator")
+  input_translate = get_translate(st.selectbox("Translation format:", ["Simplified Chinese -> English", "English -> Simplified Chinese"]))
 
-def get_rewrite():
-  input_rewrite = st.text_area("Email to rewrite: ", 
-  input_translate,  key="rewrite text area")
-  return input_rewrite
+  if st.button('Start translating', key="translate button") and input_translate:
+    st.markdown(input_translate)
 
-
-with st.sidebar:
-  st.header("Email Writer")
-  input_rewrite = get_rewrite()
-
-  if st.button('Start writing ', key="rewrite button"):
-    if input_rewrite:
-        output = generate_response(input_rewrite)
-        st.markdown(output)
-        st.markdown("""---""")   
-        st.markdown( tss.google(output, "en", "zh"))
+with col2:
+    st.header("Email Writer")
+    input_rewrite = get_rewrite()
+    if st.button('Start writing ', key="rewrite button") and input_rewrite:
+      output = generate_response(input_rewrite)
+      st.markdown(output)
+      st.markdown("""---""")   
+      st.markdown( tss.google(output, "en", "zh"))
