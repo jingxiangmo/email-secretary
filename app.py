b8bc532482b037e42fc453c45dc9ebaf62a3aec6
Jingxiang Mo <65676392+jingxiangmo@users.noreply.github.com>
2023-03-11 11:34:38 -0500

    Simplify features and UI

--- app.py
+++ app.py
@@ -2,8 +2,6 @@ import os
 import openai
 from dotenv import load_dotenv
 import streamlit as st
-import translators as ts
-import translators.server as tss
 
 load_dotenv()
 openai.api_key = os.getenv('OPENAI_API_KEY')
@@ -20,7 +18,6 @@ def generate_response(user_input: str) -> str:
   prompt= input_prompt,
   max_tokens=1000,
   )  
-
   return response.choices[0].text
 
 def generate_translation(text: str) -> str:
@@ -31,69 +28,34 @@ def generate_translation(text: str) -> str:
   )  
   return response.choices[0].text
 
-def summarize(text: str) -> str:
-  response =  openai.Completion.create(
-  model="text-davinci-003",    
-  prompt="Summarize the following into understandable bullet points in markdown in Simplified Chinese: " +  text,
-  max_tokens=1000,
-  )  
-  return response.choices[0].text
-
 # ==========[ STREAMLIT UI ]========== # 
 
 st.set_page_config(layout="wide")
 st.title("Email Secretary 📧")
-with st.sidebar:
-  st.markdown("## Navigation")
-  page = st.radio("", ["Email Writer", "Text Translate and Summary"])
 
 # =====[ EMAIL WRITER ]===== #
-if page == "Email Writer":
-  rewrite_output = ""
+rewrite_output = ""
 
-  st.markdown("## Email Writer")
-  _, col1, col2, _= st.columns(4)
-  col3, col4 = st.columns(2)
+st.markdown("## Email Writer")
+_, col1, col2, _= st.columns(4)
+col3, col4 = st.columns(2)
 
-  with col1:
-    input_lang = st.selectbox("Input Language", ["English", "Simplified Chinese"])
-  with col2:
-    output_lang = st.selectbox("Output Language:", ["English", "Simplified Chinese"])
+with col1:
+  input_lang = st.selectbox("Input Language", ["English", "中文"])
+with col2:
+  output_lang = st.selectbox("Output Language:", ["English", "中文"])
 
-  with col3:
-    st.markdown("## Input")
-    user_input = st.text_area("", height=300, key="rewrite text area")
+with col3:
+  st.markdown("## Input")
+  user_input = st.text_area("", height=300, key="rewrite text area")
 
-    if st.button('Start writing ', key="rewrite button"):
-      rewrite_output = generate_response(user_input)
-    
-  with col4:
-    st.markdown("## Output")
-    if rewrite_output: 
-      st.markdown(rewrite_output)
-      if output_lang == "English":
-        st.markdown("---")
-        st.code(generate_translation(rewrite_output), language="markdown")
-
-# =====[ EMAIL SUMMARY ]===== #
-if page == "Text Translate and Summary":
-  output_summary = ""
-
-  st.markdown("## Email Translate and Summary")
-  col5, col6 = st.columns(2)
-
-  with col5:
-    st.markdown("## Text")
-    user_input = st.text_area("", height=300, key="summary text area")
-
-    if st.button('Start Summarizing ', key="summary button"):
-      output_summary = generate_translation(user_input)
-    
-  with col6:
-    st.markdown("## Summarize")
-    if output_summary: 
-      st.markdown(output_summary)
-
+  if st.button('Start writing ', key="rewrite button"):
+    rewrite_output = generate_response(user_input)
+  
+with col4:
+  st.markdown("## Output")
+  if rewrite_output: 
+    st.markdown(rewrite_output)
+    if output_lang == "English":
       st.markdown("---")
-      st.markdown("#### 主要信息")
-      st.markdown(summarize(output_summary))
+      st.code(generate_translation(rewrite_output), language="markdown")
